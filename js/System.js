6d60cb0961d5a06a5ad0abf3b500cb618f5b5483
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-05-17 21:11:38 +0200

    Architected the Design of the Implementation stuff

---
+++ js/System.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var userController;
+var appController;
+
+// construct and execute a System setup class
+(function System() {
+  var onDeviceReady = function() {
+    alert("device ready")
+  };
+
+  var init = function() {
+    $(document).ready(function() {
+      document.addEventListener('deviceready', onDeviceReady, false);
+      userController = new UserController();
+      appController = new AppController();
+    });
+  };
+
+  init();
+})();
---
+++ js/UserController.js
@@ -0,0 +1,38 @@
+function UserController() {
+
+  var LSKEY_USERS = "UserController.users";
+
+  this.users = [];
+
+  this.loadUsers = function() {
+    var loadedUsers = JSON.parse(localStorage.getItem(LSKEY_USERS));
+    if (loadedUsers != null) {
+      this.users = loadedUsers;
+    }
+  };
+
+  this.signIn = function(username, password) {
+    alert("TODO impl signIn");
+  };
+
+  this.storeToken = function(token) {
+    alert("TODO impl storeToken");
+  };
+
+  this.delete = function(user) {
+    alert("TODO impl delete");
+  };
+
+  this._saveUsers = function() {
+    localStorage.setItem(LSKEY_USERS, JSON.stringify(this.users));
+  };
+
+  // override any defaults with user values
+  this._init = function() {
+    alert("init userController");
+    this.loadUsers();
+  };
+
+  // call this 'private' method upon class construction
+  this._init();
+}
