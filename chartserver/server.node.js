1c006d9bd7ae5016bab65817b79e998e2b095cfe
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-05-25 13:06:21 +0200

    Check in server code

---
+++ chartserver/server.node.js
@@ -0,0 +1,108 @@
+// TODO consider using 2 apps, so Android checks are not slowed down by iOS checks (and vice versa)
+var everbruggenToken = "Rt9jJoTxCgDBQrYfuHLk";
+var appid = 412598; // 'Hello World' app
+var maxSamplesForClient = 20;
+//var maxAgeOfSamplesInHours = 24; // TODO use this to limit the age of the data?
+var buildIntervalMillis = 60000 * 15; // build an app every x minutes (after the build has finished)
+var buildCheckIntervalMillis = 60000; // check the status every minute
+
+var http = require('http');
+var client = require('phonegap-build-api');
+
+// an array per platform containing arrays of [starttime, buildtime]
+var androidBuilds = [];
+var iosBuilds = [];
+
+
+// ***** webserver *****
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({
+    android: androidBuilds,
+    ios: iosBuilds
+  }));
+}).listen(9100);
+
+
+// ***** internal pgbuild checker *****
+var lastStartTimeIOS;
+var lastStartTimeAndroid;
+var lastIOSBuildComplete;
+var lastAndroidBuildComplete;
+
+function startPolling() {
+  console.log('authenticating');
+  client.auth({token: everbruggenToken}, function (e, api) {
+    buildAndCheckStatus(api);
+  });
+}
+
+function buildAndCheckStatus(api) {
+  console.log('building');
+  var now = new Date().getTime();
+  lastStartTimeIOS = now;
+  lastStartTimeAndroid = now;
+  lastIOSBuildComplete = false;
+  lastAndroidBuildComplete = false;
+  api.put('/apps/' + appid, {}, function (e, data) {
+    console.log('built');
+    setTimeout(function() {
+      checkStatus(api);
+    }, buildCheckIntervalMillis);
+  });
+}
+
+function checkStatus(api) {
+  api.get('/apps/' + appid, function (e, data) {
+    if (data == undefined) {
+      // service may be down, check later
+      setTimeout(function() {
+        checkStatus(api);
+      }, buildCheckIntervalMillis);
+    } else {
+      console.log('ios / android status: ' + data.status.ios + ' / ' + data.status.android);
+      var now = new Date().getTime();
+      if (data.status.ios == 'complete' && !lastIOSBuildComplete) {
+        lastIOSBuildComplete = true;
+        iosBuilds.push([now, (now-lastStartTimeIOS-getRandomBuildTime())/1000/60]);
+        // remove the first item from the array in case the array is now larger than the max
+        if (iosBuilds.length>maxSamplesForClient) {
+          iosBuilds.shift();
+        }
+      }
+      if (data.status.android == 'complete' && !lastAndroidBuildComplete) {
+        lastAndroidBuildComplete = true;
+        androidBuilds.push([now, (now-lastStartTimeAndroid-getRandomBuildTime())/1000/60]);
+        // remove the first item from the array in case the array is now larger than the max
+        if (androidBuilds.length>maxSamplesForClient) {
+          androidBuilds.shift();
+        }
+      }
+      var buildComplete = data.status.ios == 'complete' && data.status.android == 'complete';
+      var buildError = data.status.ios == 'error' || data.status.android == 'error';
+      if (buildError) {
+        console.log('build error');
+        setTimeout(function() {
+          buildAndCheckStatus(api);
+        }, buildIntervalMillis);
+      } else if (buildComplete) {
+        console.log('build complete');
+        setTimeout(function() {
+          buildAndCheckStatus(api);
+        }, buildIntervalMillis);
+      } else {
+        setTimeout(function() {
+          checkStatus(api);
+        }, buildCheckIntervalMillis);
+      }
+    }
+  })
+}
+
+// subtract a random amount of time, smaller than the builCheckInterval
+function getRandomBuildTime() {
+  return Math.floor((Math.random()*(buildCheckIntervalMillis-10000)));
+}
+
+// kick off!
+startPolling();
