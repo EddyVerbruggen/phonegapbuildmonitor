d5d578a8b92fa64037e4d72c402f62ee37e074ee
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-06-23 11:32:37 +0200

    no alert (prod release)

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 window.onerror = function(message, file, line) {
-  alert('Error gevangen: ' + file + ':' + line + '\n' + JSON.stringify(message));
+//  alert('Error gevangen: ' + file + ':' + line + '\n' + JSON.stringify(message));
 };
 
 function isAndroid() {
