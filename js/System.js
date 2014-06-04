36bc6bcb7f91f4ab8d24bea3d378b2744bb8bd5c
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-06-04 08:48:25 +0200

    Testing iOS8, done

--- js/System.js
+++ js/System.js
@@ -26,12 +26,6 @@ function GAStartupSuccess() {
     if (window.plugins != undefined) {
       gaPlugin = window.plugins.gaPlugin;
       gaPlugin.init(GAStartupSuccess, emptyCallback, "UA-28850866-8", 5);
-      // Testing iOS*
-      setTimeout(function() {
-        alert(navigator);
-        alert(navigator.userAgent);
-      }, 4000);
-      /*
       if (isIOS()) {
         setTimeout(function () {
           StatusBar.styleLightContent();
@@ -42,7 +36,6 @@ function GAStartupSuccess() {
           }, 250);
         }, 500);
       }
-      */
     }
   };
 
